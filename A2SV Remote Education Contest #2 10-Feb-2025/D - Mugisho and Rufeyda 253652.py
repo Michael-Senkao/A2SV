f300c87c3512dc1831c1901f56cfd6f554ae7bf2
Michael Senkao <senkaomichael@gmail.com>
2025-02-10 18:26:40 +0530

    Created 'D - Mugisho and Rufeyda' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/D - Mugisho and Rufeyda 253652.py
@@ -0,0 +1,15 @@
+# Problem: D - Mugisho and Rufeyda - https://codeforces.com/gym/586622/problem/D
+
+
+
+if __name__ == "__main__":
+    
+    n,t = map(int, input().split())
+    num = int('9' * n)
+    rem = num % t
+   
+    num = str(num - (num % t))
+    print(num if (int(num) > 0 and len(num) == n) else -1)
+    
+    
+    
