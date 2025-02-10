17c14fb861d977a674d032f3e687acf0aac2b903
Michael Senkao <senkaomichael@gmail.com>
2025-02-10 18:25:52 +0530

    Created 'B - The special paintbrush' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/B - The special paintbrush 253650.py
@@ -0,0 +1,25 @@
+# Problem: B - The special paintbrush - https://codeforces.com/gym/586622/problem/B
+
+
+
+if __name__ == "__main__":
+    t = int(input())
+    
+    for _ in range(t):
+        n = int(input())
+        s = input()
+        
+        start_index = s.find('B')
+        
+        if start_index == -1:
+            print(0)
+        else:
+            
+            end_index = start_index
+            for i in range(start_index, n):
+                if s[i] == 'B':
+                    end_index = i
+            print(end_index - start_index + 1)
+           
+    
+    
