e3f3bdc11d5ed50cb26ee2bd31b2b09f03f24cab
Michael Senkao <senkaomichael@gmail.com>
2025-02-10 18:26:04 +0530

    Created 'C - Sura loves coding' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/C - Sura loves coding 253651.py
@@ -0,0 +1,23 @@
+# Problem: C - Sura loves coding - https://codeforces.com/gym/586622/problem/C
+
+
+
+if __name__ == "__main__":
+    
+    n = int(input())
+    encoded = input()
+    
+    actual = encoded[0] if n & 1 else ''
+    index = 1 if actual else 0
+    
+    while index < n:
+        actual = encoded[index] + actual
+        index += 1
+        if index < n:
+            actual += encoded[index]
+            index += 1
+            
+    print(actual)
+           
+    
+    
