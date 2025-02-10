bac49d9d2ea26e46fa08ddda0024ffdc179bea08
Michael Senkao <senkaomichael@gmail.com>
2025-02-10 18:25:40 +0530

    Created 'A - Assem's Secret Code' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/A - Assem's Secret Code 253649.py
@@ -0,0 +1,12 @@
+# Problem: A - Assem's Secret Code - https://codeforces.com/gym/586622/problem/A
+
+
+
+if __name__ == "__main__":
+    t = int(input())
+    
+    for _ in range(t):
+       s = input()
+       
+       print("YES" if s.lower() == 'yes' else "NO")
+    
