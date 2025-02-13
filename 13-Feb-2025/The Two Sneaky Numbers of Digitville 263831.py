d3a4983a1fbe57224b9594ef551594786460cdd4
Michael Senkao <senkaomichael@gmail.com>
2025-02-14 01:22:41 +0530

    Created 'The Two Sneaky Numbers of Digitville' by A2SV Hub

---
+++ 13-Feb-2025/The Two Sneaky Numbers of Digitville 263831.py
@@ -0,0 +1,17 @@
+# Problem: The Two Sneaky Numbers of Digitville - https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/description
+
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        index = 0
+
+        # Perform cyclic sort
+        while index < n:
+            correct_index = nums[index]
+            if correct_index != index and nums[correct_index] != nums[index]:
+                nums[index], nums[correct_index] = nums[correct_index], nums[index]
+            else:
+                index += 1
+
+        # Return elements that have mismatched indices
+        return [num for index,num in enumerate(nums) if num != index]
