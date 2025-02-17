7b9226eb63266977eba7abfbe18bd7ff79251601
Michael Senkao <senkaomichael@gmail.com>
2025-02-18 00:53:05 +0530

    Created 'A - Nathan is rich' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/A - Nathan is rich 269725.py
@@ -0,0 +1,11 @@
+# Problem: A - Nathan is rich - https://codeforces.com/gym/588094/problem/A
+
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+
+    minimum_vehecle = n // 4 
+    n %= 4
+    minimum_vehecle += n // 2
+    print(minimum_vehecle)
