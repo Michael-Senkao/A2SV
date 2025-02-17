d5e59620604feba98f6d22c70e913920f4a5e725
Michael Senkao <senkaomichael@gmail.com>
2025-02-18 00:53:34 +0530

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/C - Barking Password 269730.py
@@ -0,0 +1,19 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+
+password = input()
+
+n = int(input())
+
+start_letters = set()
+end_letters = set()
+
+for _ in range(n):
+    x = input()
+    start_letters.add(x[0])
+    end_letters.add(x[1])
+    if x == password or (password[0] in end_letters and password[1] in start_letters):
+        print("YES")
+        break
+else:
+    print("NO")
