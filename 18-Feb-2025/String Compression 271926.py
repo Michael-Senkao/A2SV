77cf39165834a8f0e73af8893ec9efcf7ce5809d
Michael Senkao <senkaomichael@gmail.com>
2025-02-18 19:09:49 +0530

    Created 'String Compression' by A2SV Hub

---
+++ 18-Feb-2025/String Compression 271926.py
@@ -0,0 +1,33 @@
+# Problem: String Compression - https://leetcode.com/problems/string-compression/
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        n = len(chars)
+        count = 1
+        index = 0
+
+        for i in range(1, n):
+            if chars[i] != chars[i - 1]:
+                
+                chars[index] = chars[i - 1]
+                index += 1
+                if count > 1:
+                    count = str(count)
+                    for num in count:
+                        chars[index] = num
+                        index += 1
+
+                count = 1
+                
+            else:
+                count += 1
+
+        chars[index] = chars[-1]
+        index += 1
+        if count > 1:
+            count = str(count)
+            for num in count:
+                chars[index] = num
+                index += 1
+        
+        return index
