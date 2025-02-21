7edf3ea6fdab422d0f8aeb43aa5d79354852490b
Michael Senkao <senkaomichael@gmail.com>
2025-02-22 01:10:22 +0530

    Created 'Tuple with Same Product' by A2SV Hub

---
+++ 21-Feb-2025/Tuple with Same Product 279782.py
@@ -0,0 +1,15 @@
+# Problem: Tuple with Same Product - http://leetcode.com/problems/tuple-with-same-product
+
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+        count = defaultdict(int)
+        res = 0
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                count[nums[i] * nums[j]] += 1
+
+        for val in count.values():
+            res += ((val - 1) * val // 2)*8
+        return res
