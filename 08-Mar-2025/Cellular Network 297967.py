3316c30d34d1328ff219bbb6f29c270907f95fdf
Michael Senkao <senkaomichael@gmail.com>
2025-03-08 19:19:00 +0530

    Created 'Cellular Network' by A2SV Hub

---
+++ 08-Mar-2025/Cellular Network 297967.py
@@ -0,0 +1,28 @@
+# Problem: Cellular Network - https://codeforces.com/contest/702/problem/C
+
+
+def solve(n,m,cities,towers):
+    cities.sort()
+    towers.sort()
+    r = float('-inf')
+    towerInd = 0
+   
+    for i in range(n):
+        while towerInd < m and towers[towerInd] <= cities[i]:
+            towerInd += 1
+        
+        nearest = cities[i] - towers[towerInd - 1] if towerInd > 0 else float('inf')
+ 
+        if towerInd < m:
+            nearest = min(nearest, towers[towerInd] - cities[i])
+        r = max(r, nearest)
+        towerInd = towerInd - 1 if towerInd > 0 else towerInd
+        
+    return r
+    
+if __name__ == '__main__':
+    n,m = map(int, input().split())
+    cities = list(map(int, input().split()))
+    towers = list(map(int, input().split()))
+    
+    print(solve(n, m, cities, towers))
