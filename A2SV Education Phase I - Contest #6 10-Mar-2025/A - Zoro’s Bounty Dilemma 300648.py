bc4d4ceaeeeb8cc69c3cd180692dd8ff4c91d3fe
Michael Senkao <senkaomichael@gmail.com>
2025-03-10 23:34:17 +0530

    Created 'A - Zoro’s Bounty Dilemma' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/A - Zoro’s Bounty Dilemma 300648.py
@@ -0,0 +1,24 @@
+# Problem: A - Zoro’s Bounty Dilemma - https://codeforces.com/gym/594356/problem/A
+
+
+
+    
+if __name__ == '__main__':
+    t = int(input())
+
+    for _ in range(t):
+        s = input()
+        
+        i = 0
+        while i < len(s) and s[i] == '=':
+            i += 1
+        if i == len(s):
+            print('=')
+            continue
+        j = i
+        while j < len(s) and (s[j] == s[i] or s[j] == '='):
+            j += 1
+        if j < len(s):
+            print('?')
+        else:
+            print(s[i])
