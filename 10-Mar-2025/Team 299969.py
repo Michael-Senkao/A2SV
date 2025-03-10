f1079694ba14b325d0689ad6bc3ec102e1a32166
Michael Senkao <senkaomichael@gmail.com>
2025-03-10 15:32:16 +0530

    Created 'Team' by A2SV Hub

---
+++ 10-Mar-2025/Team 299969.py
@@ -0,0 +1,15 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+
+if __name__ == "__main__":
+    n = int(input())
+    
+    total_implemented = 0
+    for _ in range(n):
+        sure_friends = list(map(int, input().split()))
+        
+        if sum(sure_friends) > 1:
+            total_implemented += 1
+            
+    print(total_implemented)
+    
