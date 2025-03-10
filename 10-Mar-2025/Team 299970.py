2198071ea6e1c982fad2af1af5128ec04c86edc4
Michael Senkao <senkaomichael@gmail.com>
2025-03-10 15:32:42 +0530

    Created 'Team' by A2SV Hub

---
+++ 10-Mar-2025/Team 299970.py
@@ -0,0 +1,14 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+
+
+    
+if __name__ == '__main__':
+    n = int(input())
+    
+    maxSolved = 0
+    for _ in range(n):
+        knows = sum(map(int, input().split()))
+        maxSolved += knows > 1
+        
+    print(maxSolved)
