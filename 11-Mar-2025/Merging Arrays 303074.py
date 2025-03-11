a3c0f5a193fe06cac9a6186e18de7ae0fce752ae
Michael Senkao <senkaomichael@gmail.com>
2025-03-11 23:23:48 +0530

    Created 'Merging Arrays' by A2SV Hub

---
+++ 11-Mar-2025/Merging Arrays 303074.py
@@ -0,0 +1,26 @@
+# Problem: Merging Arrays - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
+
+
+
+    
+if __name__ == '__main__':
+    
+    n,m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    
+    c = []
+    i,j = 0,0
+    
+    while i < n and j < m:
+        if a[i] < b[j]:
+            c.append(a[i])
+            i += 1
+        else:
+            c.append(b[j])
+            j += 1
+    
+    c.extend(a[i:])
+    c.extend(b[j:])
+    print(*c)
+            
