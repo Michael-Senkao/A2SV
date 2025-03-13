a8238f2030243909fe596099e48e8188a627af14
Michael Senkao <senkaomichael@gmail.com>
2025-03-14 00:06:17 +0530

    Created 'Remove Element' by A2SV Hub

---
+++ 13-Mar-2025/Remove Element 305695.py
@@ -0,0 +1,12 @@
+# Problem: Remove Element - https://leetcode.com/problems/remove-element/description/
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        firstKIndex = 0
+
+        for num in nums:
+            if num != val:
+                nums[firstKIndex] = num
+                firstKIndex += 1
+
+        return firstKIndex
