b385b8856a9af94d871fa11cfb2a0883b955fe5b
Michael Senkao <senkaomichael@gmail.com>
2025-03-17 01:24:31 +0530

    Created 'Books' by A2SV Hub

---
+++ 16-Mar-2025/Books 308599.py
@@ -0,0 +1,21 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+
+    
+
+if __name__ == '__main__':
+    
+    n,t = map(int, input().split())
+    a = list(map(int, input().split()))
+    
+    left = 0
+    windowSum = 0
+    res = 0
+    for right in range(n):
+        windowSum += a[right]
+        while windowSum > t:
+            windowSum -= a[left]
+            left += 1
+        res = max(res, right - left + 1)
+        
+    print(res)
