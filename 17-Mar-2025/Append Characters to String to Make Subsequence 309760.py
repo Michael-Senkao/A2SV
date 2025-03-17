852bbdba3fa48c5fb544bba6d6e90a02a3589358
Michael Senkao <senkaomichael@gmail.com>
2025-03-18 01:30:27 +0530

    Created 'Append Characters to String to Make Subsequence' by A2SV Hub

---
+++ 17-Mar-2025/Append Characters to String to Make Subsequence 309760.py
@@ -0,0 +1,13 @@
+# Problem: Append Characters to String to Make Subsequence - https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/description/
+
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        n,m = len(s), len(t)
+        i,j = 0,0
+
+        while i < n and j < m:
+            if s[i] == t[j]:
+                j += 1
+            i += 1
+
+        return len(t) - j
