19d92d523c5b27d7c7705a0a1c6d1f1ef89da3b3
Michael Senkao <senkaomichael@gmail.com>
2025-03-21 01:19:48 +0530

    Created 'Fruit Into Baskets' by A2SV Hub

---
+++ 20-Mar-2025/Fruit Into Baskets 313742.py
@@ -0,0 +1,25 @@
+# Problem: Fruit Into Baskets - https://leetcode.com/problems/fruit-into-baskets
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        n = len(fruits)
+        nums_dict = {}
+        left = 0
+        res = 0
+
+        for right in range(n):
+            fruit = fruits[right]
+            nums_dict[fruit] = nums_dict.get(fruit, 0) + 1
+
+            while len(nums_dict) > 2:
+                nums_dict[fruits[left]] -= 1
+                if nums_dict[fruits[left]] == 0:
+                    del nums_dict[fruits[left]]
+
+                left += 1
+
+            res = max(res, right - left + 1)
+
+        return res
+
+        
