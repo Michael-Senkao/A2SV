7c3c49a5ccffe824aacac9d385f2dd0f317cf66b
Michael Senkao <senkaomichael@gmail.com>
2025-03-28 01:12:07 +0530

    Created 'B - The best for Geleta' by A2SV Hub

---
+++ A2SV Remote Contest #4 27-Mar-2025/B - The best for Geleta 320450.py
@@ -0,0 +1,29 @@
+# Problem: B - The best for Geleta - https://codeforces.com/gym/590201/problem/B
+
+import heapq
+
+t =int(input())
+
+for _ in range(t):
+    n,m = map(int, input().split())
+    arr = set(map(int, input().split()))
+    
+    maxV = max(arr)
+    
+    for _ in range(m):
+        sign,l,r = input().split()
+        l = int(l)
+        r = int(r)
+        
+        if l <= maxV <= r:
+            if sign == '-':
+                maxV -= 1
+            else:
+                maxV += 1
+
+        print(maxV, end=" ")
+
+
+    print()
+    
+
