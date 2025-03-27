4d6f419cb80b00ce572b240ba211173b71361130
Michael Senkao <senkaomichael@gmail.com>
2025-03-28 01:12:18 +0530

    Created 'E - Zombie Invasion!' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 27-Mar-2025/E - Zombie Invasion! 320451.py
@@ -0,0 +1,30 @@
+# Problem: E - Zombie Invasion! - https://codeforces.com/gym/588094/problem/E
+
+
+    
+
+if __name__ == '__main__':
+    
+    t = int(input())
+    
+    for _ in range(t):
+        n,k = map(int, input().split())
+        a = list(map(int, input().split()))
+        x = list(map(int, input().split()))
+        
+        x_to_a = [(abs(x[i]), a[i]) for i in range(n)]
+        
+        x_to_a.sort()
+        shots = 0
+      
+        
+        for time, energy in x_to_a:
+            bullets = k*time
+            if bullets - shots < energy:
+                print("NO")
+                break
+            shots += energy
+        else:
+            print("YES")
+        
+    
