c75fc0447d3c2dcd58735ee0f916f91ba9b97f9d
Michael Senkao <senkaomichael@gmail.com>
2025-03-30 23:21:51 +0530

    Created 'Maximum-sum-of-distinct-subarrays-with-length-k' by A2SV Hub

---
+++ 30-Mar-2025/Maximum-sum-of-distinct-subarrays-with-length-k 322079.py
@@ -0,0 +1,20 @@
+# Problem: Maximum-sum-of-distinct-subarrays-with-length-k - https://leetcode.com/problems/maximum-sum-of-distinct-subarrays-with-length-k/
+
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        window = set()
+
+        maxSum = 0
+        left = 0
+        sum = 0
+        for right, num in enumerate(nums):
+            sum += num
+            while num in window or right - left + 1 > k:
+                sum -= nums[left]
+                window.remove(nums[left])
+                left += 1
+
+            window.add(num)
+            if right - left + 1 == k:
+                maxSum = max(maxSum, sum)
+        return maxSum
