b44ee883487a5d36d71a2684223ebd7fbca1f6a5
Michael Senkao <senkaomichael@gmail.com>
2025-04-10 23:59:04 +0530

    Created 'Broken Keyboard' by A2SV Hub

---
+++ 10-Apr-2025/Broken Keyboard 331170.py
@@ -0,0 +1,26 @@
+# Problem: Broken Keyboard - https://codeforces.com/problemset/problem/1251/A
+
+
+if __name__ == '__main__':
+    t = int(input())
+    
+    for _ in range(t):
+        s = input()
+        
+        workingChars = set()
+        currCharCount = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                currCharCount += 1
+            else:
+                if currCharCount % 2 != 0:
+                    workingChars.add(s[i - 1])
+                    if len(workingChars) == 26:
+                        break
+                    
+                currCharCount = 1
+        
+        if currCharCount % 2 != 0:
+            workingChars.add(s[-1])
+            
+        print(''.join(sorted(workingChars)))
