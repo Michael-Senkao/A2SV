c25737774814d2218a427340fb678469737b4ff5
Michael Senkao <senkaomichael@gmail.com>
2025-04-11 01:17:43 +0530

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 10-Apr-2025/Ehab Is an Odd Person 331303.py
@@ -0,0 +1,21 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    evenCount = 0
+    oddCount = 0
+    
+    for index, val in enumerate(arr):
+        if val % 2 == 0:
+            evenCount += 1
+        else:
+            oddCount += 1
+            
+        if oddCount and evenCount:
+            arr.sort()
+            break
+        
+    print(*arr)
