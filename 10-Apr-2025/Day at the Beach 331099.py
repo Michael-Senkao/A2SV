060f60600bc4dbf8565ee134f50258eaed4da34f
Michael Senkao <senkaomichael@gmail.com>
2025-04-10 23:31:45 +0530

    Created 'Day at the Beach' by A2SV Hub

---
+++ 10-Apr-2025/Day at the Beach 331099.py
@@ -0,0 +1,23 @@
+# Problem: Day at the Beach - https://codeforces.com/problemset/problem/599/C
+
+
+if __name__ == '__main__':
+    n = int(input())
+    heights = list(map(int, input().split()))
+    
+    maxLeft = []
+    currMax = -1  
+    splits = 0
+    
+    for i in range(n):
+        maxLeft.append(currMax)
+        currMax = max(currMax, heights[i])
+        
+    currMin = float('inf')
+    
+    for i in range(n-1, -1, -1):
+        currMin = min(currMin, heights[i])
+        if currMin >= maxLeft[i]:
+            splits += 1
+            
+    print(splits)
