fdc916f4146f637ca6b63529c4c3019c70da0dde
Michael Senkao <senkaomichael@gmail.com>
2025-04-12 22:47:35 +0530

    Created 'Game of Life' by A2SV Hub

---
+++ 12-Apr-2025/Game of Life 332567.py
@@ -0,0 +1,32 @@
+# Problem: Game of Life - https://leetcode.com/problems/game-of-life/description/
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        rows,cols = len(board),len(board[0])
+        directions = [(0, 1),(0, -1),(1, 0),(1, 1),(1, -1),(-1, -1),(-1, 0),(-1, 1)]
+
+        for row in range(rows):
+            for col in range(cols):
+                ones = 0
+                for x,y in directions:
+                    dx,dy = row + x, col + y
+                    if 0 <= dx < rows and 0 <= dy < cols:
+                        ones += (board[dx][dy] & 1)
+
+                if board[row][col]:
+                    if ones < 2 or ones > 3:
+                        board[row][col] = 3 # 1 changes to 0
+                elif ones == 3:
+                    board[row][col] = 2 # 0 changes to 1
+        
+        for row in range(rows):
+            for col in range(cols):
+                if board[row][col] == 2:
+                    board[row][col] = 1
+                elif board[row][col] == 3:
+                    board[row][col] = 0
+        
