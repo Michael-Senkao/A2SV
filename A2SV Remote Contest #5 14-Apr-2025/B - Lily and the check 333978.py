e72716ed376e9bda1eeda4237c5a0502c1842f2f
Michael Senkao <senkaomichael@gmail.com>
2025-04-15 01:24:53 +0530

    Created 'B - Lily and the check' by A2SV Hub

---
+++ A2SV Remote Contest #5 14-Apr-2025/B - Lily and the check 333978.py
@@ -0,0 +1,19 @@
+# Problem: B - Lily and the check - https://codeforces.com/gym/592318/problem/B
+
+import math
+t = int(input())
+
+for _ in range(t):
+    x,y = map(int, input().split())
+    p = math.ceil(math.log(y/x, 10))
+
+   
+    res = 0
+    while y >= x:
+        max_v = x*(10**p)
+        while y >= max_v:
+            res += 1
+            y -= max_v
+        p -= 1
+    
+    print(res + y)
