da64e68b9975e6678ceddf9d1df396921aeef66b
Michael Senkao <senkaomichael@gmail.com>
2025-04-15 00:51:51 +0530

    Created 'F - Skibidus and Fanum Tax (hard version)' by A2SV Hub

---
+++ A2SV Remote Contest #5 14-Apr-2025/F - Skibidus and Fanum Tax (hard version) 333940.py
@@ -0,0 +1,45 @@
+# Problem: F - Skibidus and Fanum Tax (hard version) - https://codeforces.com/gym/592318/problem/F
+
+
+def solve(min_v, num):
+    left,right = 0, m - 1
+    bestJ = m
+
+    while left <= right:
+        mid = left + (right - left) // 2
+        if b[mid] - num < min_v:
+            left = mid + 1
+        else:
+            bestJ = mid
+            right = mid - 1
+    
+    return bestJ
+
+t = int(input())
+
+for _ in range(t):
+    n,m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    b.sort()
+
+    a[0] = min(a[0], b[0] - a[0])
+
+    for i in range(1, n):
+        bestJ = solve(a[i-1], a[i])
+        val,best = a[i], a[i]
+        if bestJ < m:
+            best = b[bestJ] - val
+            
+        if val >= a[i-1]:
+            if best >= a[i-1]:
+                a[i] = min(val, best)
+        elif best >= a[i-1]:
+            a[i] = best
+        else:
+            print('NO')
+            break
+    else:
+        print('YES')
+        
