98871d5cd24c5e61813c38e53af114c18acd81b1
Michael Senkao <senkaomichael@gmail.com>
2025-04-15 01:46:53 +0530

    Created 'D - Darth Vader's Attack' by A2SV Hub

---
+++ A2SV Remote Contest #8 14-Apr-2025/D - Darth Vader's Attack 334025.py
@@ -0,0 +1,27 @@
+# Problem: D - Darth Vader's Attack - https://codeforces.com/gym/599884/problem/D
+
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    s = input()
+
+    leftL = []
+    lCount = 0
+    res = 0
+    
+    for i in range(n):
+        if s[i] == 'L':
+            lCount += 1
+        leftL.append(lCount)
+        
+    rCount = 0
+   
+    for i in range(n-1, -1, -1):
+        if s[i] == 'R':
+            rCount += 1
+        res += (min(rCount, leftL[i]) * a[i])
+        
+    print(res)
