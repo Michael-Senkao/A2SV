0be537a12d7b8cefde9643b3af4534ba76b6060c
Michael Senkao <senkaomichael@gmail.com>
2025-04-30 22:14:29 +0530

    Created 'Longest k-Good Segment' by A2SV Hub

---
+++ 30-Apr-2025/Longest k-Good Segment 345797.py
@@ -0,0 +1,26 @@
+# Problem: Longest k-Good Segment - https://codeforces.com/problemset/problem/616/D
+
+import sys
+from collections import defaultdict
+
+#----------------- INPUT -----------------
+data = sys.stdin.read().splitlines()
+n,k = map(int, data[0].split())
+a = list(map(int, data[-1].split()))
+
+#----------------- SOLUTION -------------------
+left = 0
+start = end = 0
+freq = defaultdict(int)
+for right in range(n):
+    freq[a[right]] += 1
+    while len(freq) > k:
+        freq[a[left]] -= 1
+        if freq[a[left]] == 0:
+            del freq[a[left]]
+            
+        left += 1
+    if right - left > end - start:
+        start, end = left, right
+        
+print(start + 1, end + 1)
