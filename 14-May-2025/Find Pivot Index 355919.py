fd7f92ff81605cf6a891e0a454febbe6330103d0
Michael Senkao <senkaomichael@gmail.com>
2025-05-15 01:49:28 +0530

    Created 'Find Pivot Index' by A2SV Hub

---
+++ 14-May-2025/Find Pivot Index 355919.py
@@ -0,0 +1,23 @@
+# Problem: Find Pivot Index - https://leetcode.com/problems/find-pivot-index/description/
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 0
+
+        n = len(nums)
+        prefix_sum = []
+        running_sum = 0
+        res = -1
+
+        for num in nums:
+            prefix_sum.append(running_sum)
+            running_sum += num
+
+        suffix_sum = 0
+        for i in range(n - 1, -1, -1):
+            if suffix_sum == prefix_sum[i]:
+                res = i
+            suffix_sum += nums[i]
+        
+        return res
