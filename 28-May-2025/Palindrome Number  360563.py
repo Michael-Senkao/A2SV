7479a8ace1579b75e7a2691071cabd4c4eae8ab9
Michael Senkao <senkaomichael@gmail.com>
2025-05-29 00:38:12 +0530

    Created 'Palindrome Number ' by A2SV Hub

---
+++ 28-May-2025/Palindrome Number  360563.py
@@ -0,0 +1,17 @@
+# Problem: Palindrome Number  - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+            
+        x_str = str(x)
+        i,j = 0, len(x_str) - 1
+
+        while i < j:
+            if x_str[i] != x_str[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
