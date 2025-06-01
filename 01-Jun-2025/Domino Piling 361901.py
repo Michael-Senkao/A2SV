9f48f30863baab184903239a914bbb40b1ab6082
Michael Senkao <senkaomichael@gmail.com>
2025-06-01 23:15:52 +0530

    Created 'Domino Piling' by A2SV Hub

---
+++ 01-Jun-2025/Domino Piling 361901.py
@@ -0,0 +1,11 @@
+# Problem: Domino Piling - https://codeforces.com/problemset/problem/50/A
+
+
+m,n = map(int, input().split())
+
+
+ans = (n // 2) * m
+if n % 2 != 0:
+    ans += m // 2
+
+print(ans)
