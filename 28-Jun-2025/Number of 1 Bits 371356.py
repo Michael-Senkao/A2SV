abc8682b37d61bdf49288401ff180891ee893443
Michael Senkao <senkaomichael@gmail.com>
2025-06-28 14:21:51 +0530

    Created 'Number of 1 Bits' by A2SV Hub

---
+++ 28-Jun-2025/Number of 1 Bits 371356.py
@@ -0,0 +1,8 @@
+# Problem: Number of 1 Bits - https://leetcode.com/problems/number-of-1-bits/description/
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        if n == 0:
+            return 0
+    
+        return (n & 1) + self.hammingWeight(n >> 1)
