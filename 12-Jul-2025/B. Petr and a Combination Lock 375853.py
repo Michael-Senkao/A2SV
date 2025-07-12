796ddcd31c207a50c37aacbd442280e64ba2fe00
Michael Senkao <senkaomichael@gmail.com>
2025-07-12 20:19:54 +0000

    Created 'B. Petr and a Combination Lock' by A2SV Hub

---
+++ 12-Jul-2025/B. Petr and a Combination Lock 375853.py
@@ -0,0 +1,17 @@
+# Problem: B. Petr and a Combination Lock - https://codeforces.com/contest/1097/problem/B
+
+
+from collections import deque
+
+def dfs(pos, indx, degrees):
+    if indx >= len(degrees):
+        return pos == 0
+    return dfs((pos + degrees[indx]) % 360, indx + 1, degrees) or dfs((pos - degrees[indx] + 360) % 360, indx + 1, degrees)
+
+
+n = int(input())
+
+degrees = []
+for _ in range(n):
+    degrees.append(int(input()))
+print('YES' if dfs(0, 0, degrees) else 'NO')
