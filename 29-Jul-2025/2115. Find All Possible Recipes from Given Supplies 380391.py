64a1f56a9ab97217fb6136e6dbaefb7b62e539b8
Michael Senkao <senkaomichael@gmail.com>
2025-07-29 19:15:02 +0000

    Created '2115. Find All Possible Recipes from Given Supplies' by A2SV Hub

---
+++ 29-Jul-2025/2115. Find All Possible Recipes from Given Supplies 380391.py
@@ -0,0 +1,31 @@
+# Problem: 2115. Find All Possible Recipes from Given Supplies - https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/description/
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        
+        supplies = set(supplies)
+        graph = defaultdict(list)
+        indegree = defaultdict(int)
+
+        for i in range(len(recipes)):
+            recipe = recipes[i]
+            for ingre in ingredients[i]:
+                graph[ingre].append(recipe)
+                indegree[recipe] += 1
+
+        q = deque()
+        for recipe, ingre in graph.items():
+            if indegree[recipe] == 0 and recipe in supplies:
+                q.append(recipe)
+
+        canMake = []
+        while q:
+            ingre = q.popleft()
+            for recipe in graph[ingre]:
+                indegree[recipe] -= 1
+                if indegree[recipe] == 0:
+                    q.append(recipe)
+                    canMake.append(recipe)
+
+
+        return canMake
