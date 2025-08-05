48836b566e4541eb0b171cb1594969b1ccef25d6
Michael Senkao <senkaomichael@gmail.com>
2025-08-05 20:19:42 +0000

    Created 'Pascal's Triangle II - LeetCode' by A2SV Hub

---
+++ 05-Aug-2025/Pascal's Triangle II - LeetCode 382242.py
@@ -0,0 +1,15 @@
+# Problem: Pascal's Triangle II - LeetCode - https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution:
+   
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+        
+        for _ in range(numRows - 1):
+            currRow = [1]
+            prevRow = res[-1]
+            for i in range(1, len(prevRow)):
+                currRow.append(prevRow[i] + prevRow[i-1])
+            currRow.append(1)
+            res.append(currRow)
+        return res
