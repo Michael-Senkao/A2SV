d51e4fe2ba67acec75f78fc240e6bdac56ffe7b7
Michael Senkao <senkaomichael@gmail.com>
2025-08-08 15:46:38 +0000

    Created 'N-th Tribonacci Number' by A2SV Hub

---
+++ 08-Aug-2025/N-th Tribonacci Number 382848.py
@@ -0,0 +1,15 @@
+# Problem: N-th Tribonacci Number - https://leetcode.com/problems/n-th-tribonacci-number/description/
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n < 3:
+            return 1
+
+        dp = [0] * (n + 1)
+        dp[1] = dp[2] = 1
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        
+        return dp[n] 
