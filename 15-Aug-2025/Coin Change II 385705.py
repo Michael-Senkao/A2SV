0e33aa79eebf3fd7a294e5ba14b535f572a87415
Michael Senkao <senkaomichael@gmail.com>
2025-08-15 21:05:41 +0000

    Created 'Coin Change II' by A2SV Hub

---
+++ 15-Aug-2025/Coin Change II 385705.py
@@ -0,0 +1,17 @@
+# Problem: Coin Change II - https://leetcode.com/problems/coin-change-ii/
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        memo = {}
+        def dp(index, curr_sum):
+            if curr_sum == amount:
+                return 1
+            if index >= n or curr_sum > amount:
+                return 0
+            if (index, curr_sum) in memo:
+                return memo[(index, curr_sum)]
+            memo[(index, curr_sum)] = dp(index, curr_sum + coins[index]) + dp(index+1, curr_sum)
+            return memo[(index, curr_sum)]
+
+        n = len(coins)
+        return dp(0, 0)
