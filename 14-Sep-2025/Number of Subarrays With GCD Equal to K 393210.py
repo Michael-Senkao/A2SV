fdbb8b137b76b968917e7efa0ed62a4d07db4c08
Michael Senkao <senkaomichael@gmail.com>
2025-09-14 21:12:25 +0000

    Created 'Number of Subarrays With GCD Equal to K' by A2SV Hub

---
+++ 14-Sep-2025/Number of Subarrays With GCD Equal to K 393210.py
@@ -0,0 +1,18 @@
+# Problem: Number of Subarrays With GCD Equal to K - https://leetcode.com/problems/number-of-subarrays-with-gcd-equal-to-k/description/
+
+class Solution:
+    def subarrayGCD(self, nums: List[int], k: int) -> int:
+        def gcd(a,b):
+            return a if b == 0 else gcd(b, a%b)
+
+        n = len(nums)
+        count = 0
+
+        for i in range(n):
+            curr_gcd = 0
+            for j in range(i, n):
+                curr_gcd = gcd(curr_gcd, nums[j])
+                count += curr_gcd == k
+                if curr_gcd < k: break
+
+        return count
