5b937860d821bcccd9e66d49b908ad18e0d8abcf
Michael Senkao <senkaomichael@gmail.com>
2025-10-06 23:24:28 +0000

    Created 'Network Delay Time' by A2SV Hub

---
+++ 06-Oct-2025/Network Delay Time 400448.py
@@ -0,0 +1,30 @@
+# Problem: Network Delay Time - https://leetcode.com/problems/network-delay-time/description/
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        costs = {node: float("inf") for node in range(1, n+1)} # Stores the cost to reach each node in the network
+        
+        # Represent the network as a graph
+        for u,v,w in times:
+            graph[u].append((v,w))
+
+        visited = set()
+        heap = [(0, k)] #(cost, node)
+        heapify(heap)
+        costs[k] = 0
+        
+
+        while heap:
+            current_cost, current_node = heappop(heap)
+            if current_node in visited:
+                continue
+            costs[current_node] = current_cost
+            visited.add(current_node) 
+
+            for neighbor, cost in graph[current_node]:
+                heappush(heap, (current_cost + cost, neighbor))
+
+        if len(visited) != n: # All nodes cannot be visited
+            return -1
+        return max(costs.values())
