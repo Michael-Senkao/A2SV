095afe5570536df8541ec41dfd4fc35445b4f360
Michael Senkao <senkaomichael@gmail.com>
2025-10-07 14:19:11 +0000

    Created 'Top K Frequent Words' by A2SV Hub

---
+++ 07-Oct-2025/Top K Frequent Words 400539.py
@@ -0,0 +1,13 @@
+# Problem: Top K Frequent Words - https://leetcode.com/problems/top-k-frequent-words/
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        heap = [(-frequency, word) for word,frequency in Counter(words).items()]
+        heapify(heap)
+        ans = []
+
+        while k:
+            _,word = heappop(heap)
+            ans.append(word)
+            k -= 1
+        return ans
